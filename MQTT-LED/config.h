e46ee75e745ffdfd76b996777788f83453e0a4b7
YUZHEN200495 <57867205+YUZHEN200495@users.noreply.github.com>
2024-02-27 17:25:51 +0100

    Create config.h

---
+++ MQTT-LED/config.h
@@ -0,0 +1,21 @@
+#define BOARD "ESP8266"
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "password"
+#define LAN_ADDRESS "10.0.0.3"
+#define LAN_GATEWAY "10.0.0.1"
+#define LAN_SUBNET "255.255.0.0"
+
+#define MQTT_ADDRESS "10.0.0.2"
+#define MQTT_PORT 1883
+#define MQTT_USERNAME "username"
+#define MQTT_PASSWORD "password"
+
+#define MQTT_DISCOVERY "homeassistant/light/bedroom/bedlamp/config"
+#define MQTT_BASE "bedroom/bedlamp/"
+
+#define DEVICE_NAME "Bed Lamp"
+#define DEVICE_ID "bedlamp"
+
+#define TRANSITION_SPEED 255.f/1000.f
+#define LAMP_PIN 4
